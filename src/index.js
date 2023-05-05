b6de9897662e3bf7a1d6515ff2182b20a0396544
Kobi Jones <kobijonesmail@gmail.com>
2023-05-04 20:12:23 -0500

    Add redux store and feedback reducer

--- src/index.js
+++ src/index.js
@@ -2,10 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
+import { applyMiddleware, combineReducers, legacy_createStore as createStore } from 'redux';
+import { feedback } from './redux/reducers';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+const store = createStore(
+    combineReducers({
+        feedback
+    }),
+    applyMiddleware(logger)
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
---
+++ src/redux/actions/feedback.js
@@ -0,0 +1,6 @@
+export const setFeedback = feedback => {
+  return {
+    type: 'feedback/set',
+    payload: feedback
+  };
+};
