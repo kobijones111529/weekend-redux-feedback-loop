e84ed6191e6f6637f23fa17e6a02b8b097a4be72
Kobi Jones <kobijonesmail@gmail.com>
2023-05-07 15:07:01 -0500

    Add navigation from success page to home

--- src/components/SuccessPage/SuccessPage.jsx
+++ src/components/SuccessPage/SuccessPage.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom/cjs/react-router-dom";
+
 function SuccessPage() {
   return (
     <>
       <p>Thanks!</p>
+      <Link to="/">Leave new feedback</Link>
     </>
   );
 }
